0ba70f6b8d78fadcb58045d14229f436df555088
Stanley Liu <sliu1993@hotmail.co.uk>
2020-02-25 11:13:49 +0000

    created new and create methods in controller and a #new view

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -1,7 +1,20 @@
 class BicyclesController < ApplicationController
   def new
+    @bicycle = Bicycle.new
   end
 
   def create
+    @bicycle = Bicycle.new(bicycle_params)
+    if @bicycle.save
+      redirect_to bicycles_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bicycle_params
+    params.require(:bicycle).permit(:location, :style, :price, :size, :title)
   end
 end
