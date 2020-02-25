29f54d0b84e97563706f988cc1e5f73379207c06
danielle levy <danielle.levyy@gmail.com>
2020-02-25 15:03:47 +0000

    working on the layout page of showpage made a scss

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,6 +1,7 @@
 class Bicycle < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
+  has_many :reviews #added has_many to bicycles
   belongs_to :user
   validates :title, presence: true
   validates :size, presence: true
