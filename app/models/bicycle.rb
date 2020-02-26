0d08b2811ae58cb74c5475e3a7616a7cf2f55129
danielle levy <danielle.levyy@gmail.com>
2020-02-26 16:16:19 +0000

    done

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,12 +1,12 @@
 class Bicycle < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
-  has_many :reviews #added has_many to bicycles
+  has_many :reviews # added has_many to bicycles
   belongs_to :user
   validates :title, presence: true
   validates :size, presence: true
   validates :style, presence: true
   validates :location, presence: true
   validates :price, presence: true, numericality: true
-  has_one_attached :photo #for cloudinary
+  has_one_attached :photo # for cloudinary
 end
