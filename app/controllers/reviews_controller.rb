484c9b27bbeb787774fe18db04c2d68d03c93761
Maxime Seiter <maxycle@hotmail.com>
2020-02-26 15:54:30 +0000

    reviews done, delete doesn't work yet

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,10 @@
 class ReviewsController < ApplicationController
   before_action :set_bicycle
 
+  def index
+  	@reviews = Review.where(bicycle_id: params[:bicycle_id])
+  end
+
   def show
     @review = Review.find(params[:id])
   end
@@ -12,11 +16,19 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.bicycle = @bicycle
+    @review.user = current_user
     if @review.save
-      redirect_to bicycle_review_path([@bicycle, @review])
+      redirect_to bicycle_review_path(@bicycle, @review)
     else
       render :new
     end
+  end
+
+  def destroy
+  	@review = Review.find(params[:id])
+  	@review.destroy
+ # 	redirect_to bicycle_reviews_path(@bicycle)
+  	redirect_to bicycles_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :bicycles do
     resources :bookings, only: [:show, :new, :create]
-    resources :reviews, only: [:show, :new, :create]
+    resources :reviews, only: [:show, :new, :create, :index]
   end
+  resources :reviews, only: [:destroy]
 end
