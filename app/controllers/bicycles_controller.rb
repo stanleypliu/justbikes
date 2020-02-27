f83bbd036798ef8b394c24683e54fd4fa13158f4
Maxime Seiter <maxycle@hotmail.com>
2020-02-27 15:48:59 +0000

    review show page css and deleting review and authorizing done

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -1,4 +1,6 @@
 class BicyclesController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @bicycle = Bicycle.where.not(user: current_user) # show bicycles user doesnt own
     # @bicycle.select{ |bicycle| } do be worked on tomorrow
